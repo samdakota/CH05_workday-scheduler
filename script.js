aed283d437b3b8c87aa994e5137aeab4c99c8372
Sam Miller <samdakota@gmail.com>
2022-03-29 11:22:45 -0400

    starting code

---
+++ script.js
@@ -0,0 +1,27 @@
+// display current day at the top
+// generate timeblocks for all hours of a day
+// timeblocks can be edited to add text information
+// save button next to timeblock saves to local storage
+var today = 
+var currentTime =
+
+var timeBlockContainer = getElementById('time-container');
+
+
+var createTimeBlock = function() {
+
+}
+
+var timeTask = 
+
+var timeBlockFormHandler() {
+
+}
+
+var saveTimeBlock = function() {
+
+}
+
+// generate timeblock function (start with time, check against rules, fill with info from localstorage or empty, append to DOM)
+// loop through all time blocks and add class based on time
+// form input for each time block, submit saves
